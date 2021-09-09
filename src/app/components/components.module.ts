7c60c060c3c2dcac1e1919da8c5cc261778d8855
TH <tomashilbert@hotmail.com>
2021-09-08 23:25:28 -0500

    Component modules y Ion-Lists

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class ComponentsModule { }
