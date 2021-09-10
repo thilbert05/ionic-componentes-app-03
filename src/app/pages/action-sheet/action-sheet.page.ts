99502fd1f763cade5029cc2392c398b6ec040aca
TH <tomashilbert@hotmail.com>
2021-09-10 00:32:28 -0500

    Ion alert y Ion Action Sheet

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-action-sheet',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionSheetPage implements OnInit {
 
-  constructor() { }
+  constructor(private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
+  }
+
+  async mostrarActionSheet() {
+    const actionSheetEl = await this.actionSheetCtrl.create({
+      header: 'Albumes',
+      backdropDismiss: false,
+      cssClass: 'action-sheet',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash-outline',
+          cssClass: 'delete-option',
+          handler: () => {
+            console.log('Delete clicked');
+          },
+        },
+        {
+          text: 'Share',
+          icon: 'share-outline',
+          handler: () => {
+            console.log('Share clicked');
+          },
+        },
+        {
+          text: 'Play (open modal)',
+          icon: 'caret-forward-circle-outline',
+          handler: () => {
+            console.log('Play clicked');
+          },
+        },
+        {
+          text: 'Favorite',
+          icon: 'heart-outline',
+          handler: () => {
+            console.log('Favorite clicked');
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+      ],
+    });
+    await actionSheetEl.present();
+
+    const { role } = await actionSheetEl.onDidDismiss();
+
+    console.log('Role: ', role);
+
   }
 
 }
--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,164 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertCtrl: AlertController) { }
 
   ngOnInit() {
+
+  }
+
+  async showAlert() {
+    const alertEl = await this.alertCtrl.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      backdropDismiss: false,
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Cliked Ok.');
+          }
+        }
+      ]
+    });
+
+    await alertEl.present();
+  }
+
+  async presentAlertMultipleButtons() {
+    const alertEl = await this.alertCtrl.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Click en OK!');
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'delete-option',
+          handler: () => {
+            console.log('Click en cancel!');
+          },
+        },
+      ],
+    });
+
+    await alertEl.present();
+
+    const {role} = await alertEl.onDidDismiss();
+    console.log('Role seleccionado: ', role);
+  }
+
+  async presentAlertConfirm() {
+    const alertEl = await this.alertCtrl.create({
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alertEl.present();
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertCtrl.create({
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Placeholder 1'
+        },
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          value: 'hello',
+          placeholder: 'Placeholder 2'
+        },
+        // multiline input.
+        {
+          name: 'paragraph',
+          id: 'paragraph',
+          type: 'textarea',
+          placeholder: 'Placeholder 3'
+        },
+        {
+          name: 'name3',
+          value: 'http://ionicframework.com',
+          type: 'url',
+          placeholder: 'Favorite site ever'
+        },
+        // input date with min & max
+        {
+          name: 'name4',
+          type: 'date',
+          min: '2020-03-01',
+          max: '2022-01-12'
+        },
+        // input date without min nor max
+        {
+          name: 'name5',
+          type: 'date'
+        },
+        {
+          name: 'name6',
+          type: 'number',
+          min: -5,
+          max: 10
+        },
+        {
+          name: 'name7',
+          type: 'number'
+        },
+        {
+          name: 'name8',
+          type: 'password',
+          placeholder: 'Advanced Attributes',
+          cssClass: 'specialClass',
+          attributes: {
+            maxlength: 4,
+            inputmode: 'decimal'
+          }
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data: any) => {
+            console.log('Confirm data: ', data);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
